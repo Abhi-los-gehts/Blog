e7fcad2a179b36f1ffb88535f256d335b2363f7c
Abhilasha <Abhilasha.reva61@gmail.com>
2025-03-20 15:40:34 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+export default function Page() {
+    return <> <h1 className="text-gray-300">Hello, Next.js!</h1>
+    <p className="font-bold dark:text-red-400 underline"> its testing phase </p>
+     <p className="font-medium text-red-500">Product Engineer</p>
+    </>
+  }
